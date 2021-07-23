be105cc3743559b4e2994572883ebf7198afbf43
AnnaHry <anjahrynda@gmail.com>
2021-07-23 01:36:21 +0000

    store js

---
+++ js/store.js
@@ -0,0 +1,36 @@
+const productsJson =[
+    {
+        "id": "1",
+        "imageUrl": "img/types-of-honey/wildflower-honey.jpg",
+        "title": "Wildflower Honeyy",
+        "price": 7.000 USD
+    }
+    {
+        "id": "2",
+        "imageUrl": "img/types-of-honey/blossom-honey.jpg",
+        "title": "Blossom Honey",
+        "price": 20.000 
+    }
+    {
+        "id": "3",
+        "imageUrl": "img/types-of-honey/clover-honey.jpg",
+        "title": "Clover Honey",
+        "price":  8.000 
+    }
+    {
+        "id": "4",
+        "imageUrl": "img/types-of-honey/buckwheat-honey.jpg",
+        "title": "Buckwheat Honey",
+        "price": 10.000 
+    }
+    {
+        "id": "5",
+        "imageUrl": "img/types-of-honey/citrus-blossom-honey.jpg",
+        "title": "Citrus Blossom Honey",
+        "price": 15.000 
+    }
+];
+const products = JSON.parse(productsJson);
+function renderProducts(products){
+
+}
