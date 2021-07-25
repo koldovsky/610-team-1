419e3fcb0445f7f85077d8e9c15524d87ffe9fd5
AnnaHry <anjahrynda@gmail.com>
2021-07-25 23:01:05 +0000

    store corrected

--- js/store.js
+++ js/store.js
@@ -1,6 +1,7 @@
-const productsJson =[
+const productsJson =`[
     {
         "id": "1",
+        "pages": "wildflower-honey.html",
         "imageUrl": "img/types-of-honey/wildflower-honey.jpg",
         "title": "Wildflower Honeyy",
         "price": 7.000,
@@ -8,6 +9,7 @@ const productsJson =[
     },
     {
         "id": "2",
+        "pages": "wildflower-honey.html",
         "imageUrl": "img/types-of-honey/blossom-honey.jpg",
         "title": "Blossom Honey",
         "price": 20.000,
@@ -15,6 +17,7 @@ const productsJson =[
     },
     {
         "id": "3",
+        "pages": "wildflower-honey.html",
         "imageUrl": "img/types-of-honey/clover-honey.jpg",
         "title": "Clover Honey",
         "price":  8.000,
@@ -22,6 +25,7 @@ const productsJson =[
     },
     {
         "id": "4",
+        "pages": "wildflower-honey.html",
         "imageUrl": "img/types-of-honey/buckwheat-honey.jpg",
         "title": "Buckwheat Honey",
         "price": 10.000, 
@@ -29,23 +33,25 @@ const productsJson =[
     },
     {
         "id": "5",
+        "pages": "wildflower-honey.html",
         "imageUrl": "img/types-of-honey/citrus-blossom-honey.jpg",
         "title": "Citrus Blossom Honey",
         "price": 15.000,
         "button": "Buy Now" 
     }
-];
+]`;
 const product = JSON.parse(productsJson);
 function renderProducts(products){
     let html = "";
     for (const product of products) {
         html += `<div class="slide-shop">
-        <img class="img-carousel-two" src="${products.imageUrl}" alt="wildflower-honey"/></a>
-        <h4>${products.title}</h4>
-        <h5>${products.price}USA </h5>
-        <a href="#store" class="button-outline-button">${products.button}</a>
+        <a href="${product.pages}" target="_blank">
+        <img class="img-carousel-two" src="${product.imageUrl}" alt="wildflower-honey"/></a>
+        <h4>${product.title}</h4>
+        <h5>$${product.price}USA </h5>
+        <a href="#store" class="button-outline-button">${product.button}</a>
         </div>`;
     }
-    document.querySelector('.product').innerHTML = html;
+    document.querySelector('.carousel-two').innerHTML = html;
 }
 renderProducts(product);
