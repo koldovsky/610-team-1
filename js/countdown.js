315b3401be6812aabf57f49f0725784a45d0a8dc
Zahranovskyi <mzahranovskyi@gmail.com>
2021-07-25 18:59:18 +0000

    Uptdated countdown

--- js/countdown.js
+++ js/countdown.js
@@ -11,7 +11,10 @@ const x = setInterval(function() {
   const minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
   const seconds = Math.floor((distance % (1000 * 60)) / 1000);
 
-  document.getElementById("countdownS").innerHTML = days + " Days " + hours + " Hours " + minutes + " Minutes " + seconds + " Seconds ";
+document.getElementById("countdown-one").innerHTML = days;
+document.getElementById("countdown-two").innerHTML = hours;
+document.getElementById("countdown-three").innerHTML = minutes;
+document.getElementById("countdown-four").innerHTML = seconds;
 
 }, 1000);
 
