345f7e42936d7731e7510f6cbb9a34f776d2dbc6
AnnaHry <anjahrynda@gmail.com>
2021-07-27 23:26:52 +0000

    added products.json

--- js/store.js
+++ js/store.js
@@ -1,46 +1,19 @@
-const productsJson =`[
-    {
-        "id": "1",
-        "pages": "wildflower-honey.html",
-        "imageUrl": "img/types-of-honey/wildflower-honey.jpg",
-        "title": "Wildflower Honeyy",
-        "price": " 7.000 ",
-        "button": "Buy Now"
-    },
-    {
-        "id": "2",
-        "pages": "wildflower-honey.html",
-        "imageUrl": "img/types-of-honey/blossom-honey.jpg",
-        "title": "Blossom Honey",
-        "price": " 20.000 ",
-        "button": "Buy Now"
-    },
-    {
-        "id": "3",
-        "pages": "wildflower-honey.html",
-        "imageUrl": "img/types-of-honey/clover-honey.jpg",
-        "title": "Clover Honey",
-        "price":  " 8.000 ",
-        "button": "Buy Now" 
-    },
-    {
-        "id": "4",
-        "pages": "wildflower-honey.html",
-        "imageUrl": "img/types-of-honey/buckwheat-honey.jpg",
-        "title": "Buckwheat Honey",
-        "price": " 10.000 ", 
-        "button": "Buy Now"
-    },
-    {
-        "id": "5",
-        "pages": "wildflower-honey.html",
-        "imageUrl": "img/types-of-honey/citrus-blossom-honey.jpg",
-        "title": "Citrus Blossom Honey",
-        "price": " 15.000 ",
-        "button": "Buy Now" 
-    }
-]`;
-const product = JSON.parse(productsJson);
+//function loadProducts () {
+//    fetch('products.json')
+//        .then( response => response.json() )
+//        .then (productsData => {
+//            products = productsData;
+//            renderProducts (products, 'ascending');
+//        });
+//}
+
+async function loadProducts () {
+    const response = await fetch('products.json');
+    products = await response.json();
+    renderProducts (products, 'ascending');
+}
+
+
 function renderProducts(products){
     let html = "";
     for (const product of products) {
@@ -54,5 +27,5 @@ function renderProducts(products){
     }
     document.querySelector('.carousel-two').innerHTML = html;
 }
-renderProducts(product);
+loadProducts();
 
