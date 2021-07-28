72a93996c42928f67637230df065ae484dc1599c
AnnaHry <anjahrynda@gmail.com>
2021-07-28 00:43:10 +0000

    store

--- js/carousel.js
+++ js/carousel.js
@@ -1,9 +1,18 @@
-const slides = `[productsJson]`; 
+(function () {
+    const slides = [productsJson]; 
 
 let currentSlide = 0;
 function showCurrentSlide() {
-    const slideTarget = document.querySelector('.carousel-two"')
-    slideTarget.src = slides[currentSlide];
+    //const slideTarget = document.querySelector('.carousel-two"')
+    //slideTarget.src = slides[currentSlide];
+    const slideImage = document.querySelector('.carousel-two"')
+    slideImage.src = product[currentSlide].imageUrl;
+    const slideTitle = document.querySelector('.carousel-two"')
+    slideTitle.innerText = product[currentSlide].title;
+    const slideTitlenext = document.querySelector('.carousel-two"')
+    slideTitlenext.innerText = product[currentSlide].price;
+    const slideButton= document.querySelector('.carousel-two"')
+    slideButton.innerHTML = product[currentSlide].button;
 }
 
 function nextSlide() {
@@ -23,3 +32,4 @@ document.querySelector('.bprev').addEventListener('click', nextSlide);
 
 showCurrentSlide();
 setInterval(nextSlide, 3000);
+})();
