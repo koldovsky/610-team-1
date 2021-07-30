06ab2d3307f407e6f9badc7e70b9854148ed38e8
reed01010101 <reed01010101@gmail.com>
2021-07-30 18:09:37 +0000

    clock fixed

--- js/time.js
+++ js/time.js
@@ -1,2 +1,7 @@
-const dt = new Date();
-document.getElementById("datetime").innerHTML = dt.toLocaleTimeString();
+function time(){
+    let dt = new Date();
+    document.getElementById("datetime").innerHTML = dt.toLocaleTimeString();
+    setTimeout(time, 1000);
+    }
+    time();
+    
